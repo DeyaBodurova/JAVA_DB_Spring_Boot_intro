d1d7a5a0994ec431a358e52c8c011faffca0d32e
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-05 14:22:33 +0200

    wip

--- src/main/java/com/example/bookshop/domain/entities/Author.java
+++ src/main/java/com/example/bookshop/domain/entities/Author.java
@@ -2,8 +2,11 @@ package com.example.bookshop.domain.entities;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -12,5 +15,11 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @Entity
 @Table
-public class Author extends  BaseEntity{
+public class Author extends BaseEntity {
+    @Column(name = "fist_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @OneToMany
+    private Set<Book> books;
 }
