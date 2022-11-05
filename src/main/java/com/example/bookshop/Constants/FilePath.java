9a874f76d21c9d494eb17ed63af420126166c69d
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-05 16:22:32 +0200

    wip

--- src/main/java/com/example/bookshop/Constants/FilePath.java
+++ src/main/java/com/example/bookshop/Constants/FilePath.java
@@ -2,8 +2,8 @@ package com.example.bookshop.Constants;
 
 public enum FilePath {
     ;
-    static final String RESOURCE_URL = "src/main/resources/dbContent/"
-    static final String BOOK_FILE_NAME = "BOOKS"
-    static final String CATEGORY_FILE_NAME = "src/main/resources/dbContent/"
-    static final String AUTHOR_FILE_NAME = "src/main/resources/dbContent/"
+    static final String RESOURCE_URL = "src/main/resources/dbContent/";
+    static final String BOOK_FILE_NAME = "books.txt";
+    static final String CATEGORY_FILE_NAME = "categories.txt";
+    static final String AUTHOR_FILE_NAME = "authors.txt";
 }
