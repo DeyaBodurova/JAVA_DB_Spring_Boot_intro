dcf6c203c61e9ac24814c5052f5c26505d2dc016
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-05 14:45:27 +0200

    wip

--- src/main/java/com/example/bookshop/repositories/BookRepository.java
+++ src/main/java/com/example/bookshop/repositories/BookRepository.java
@@ -1,4 +1,9 @@
 package com.example.bookshop.repositories;
 
-public class BookRepository {
+import com.example.bookshop.domain.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
--- src/main/java/com/example/bookshop/repositories/CategoryRepository.java
+++ src/main/java/com/example/bookshop/repositories/CategoryRepository.java
@@ -1,4 +1,11 @@
 package com.example.bookshop.repositories;
 
-public class CategoryRepository {
+import com.example.bookshop.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Locale;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
