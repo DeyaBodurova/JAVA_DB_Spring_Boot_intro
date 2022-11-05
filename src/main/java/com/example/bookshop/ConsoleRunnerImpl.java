2cffcb19e43b703c6cd9c00a6bd9a053900b0879
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-05 15:04:00 +0200

    wip

--- src/main/java/com/example/bookshop/ConsoleRunnerImpl.java
+++ src/main/java/com/example/bookshop/ConsoleRunnerImpl.java
@@ -1,8 +1,22 @@
 package com.example.bookshop;
 
+import com.example.bookshop.repositories.AuthorRepository;
+import com.example.bookshop.repositories.BookRepository;
+import com.example.bookshop.repositories.CategoryRepository;
 import org.springframework.boot.CommandLineRunner;
 
 public class ConsoleRunnerImpl implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final CategoryRepository categoryRepository;
+
+    public ConsoleRunnerImpl(AuthorRepository authorRepository, CategoryRepository categoryRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.categoryRepository = categoryRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    private final BookRepository bookRepository;
     @Override
     public void run(String... args) throws Exception {
 
