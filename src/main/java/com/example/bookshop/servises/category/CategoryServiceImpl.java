18cecc9b0ed2a89d60020d04ecc3264744755a5c
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-05 15:58:09 +0200

    wip

---
+++ src/main/java/com/example/bookshop/servises/category/CategoryServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.bookshop.servises.category;
+
+import com.example.bookshop.domain.entities.Category;
+import com.example.bookshop.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void seedCategories(List<Category> categories) {
+        this.categoryRepository.saveAll(categories);
+    }
+}
--- src/main/java/com/example/bookshop/servises/seed/SeedServiceImpl.java
+++ src/main/java/com/example/bookshop/servises/seed/SeedServiceImpl.java
@@ -1,4 +1,33 @@
 package com.example.bookshop.servises.seed;
 
-public class SeedServiceImpl {
+import com.example.bookshop.servises.book.BookService;
+import com.example.bookshop.servises.category.CategoryService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SeedServiceImpl implements SeedService {
+    private final SeedService seedService;
+    private final BookService bookService;
+    private final CategoryService categoryService;
+
+    public SeedServiceImpl(SeedService seedService, BookService bookService, CategoryService categoryService) {
+        this.seedService = seedService;
+        this.bookService = bookService;
+        this.categoryService = categoryService;
+    }
+
+    @Override
+    public void seedAuthors() {
+
+    }
+
+    @Override
+    public void seedBooks() {
+
+    }
+
+    @Override
+    public void seedCategory() {
+
+    }
 }
