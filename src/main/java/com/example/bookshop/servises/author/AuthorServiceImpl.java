a24e529a7a25f01debca839067eccd89fd3191cf
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-07 20:30:27 +0200

    wip

--- src/main/java/com/example/bookshop/servises/author/AuthorServiceImpl.java
+++ src/main/java/com/example/bookshop/servises/author/AuthorServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @Service
 public class AuthorServiceImpl implements AuthorService {
+
     private final AuthorRepository authorRepository;
 
     @Autowired
@@ -19,6 +20,34 @@ public class AuthorServiceImpl implements AuthorService {
     @Override
     public void seedAuthors(List<Author> authors) {
         this.authorRepository.saveAll(authors);
+    }
+
+    @Override
+    public boolean isDataSeeded() {
+        return this.authorRepository.count() > 0;
+    }
+
+    @Override
+    public Author getRandomAuthor() {
+        final long count = this.authorRepository.count();
+
+        if (count != 0) {
+            final long randomAuthorId = new Random().nextLong(1L, count) + 1L;
+
+            return this.authorRepository.findAuthorById(randomAuthorId).orElseThrow(NoSuchElementException::new);
+        }
+
+        throw new RuntimeException();
+    }
+
+    @Override
+    public List<Author> findDistinctByBooksBefore(LocalDate date) {
+        return this.authorRepository.findDistinctByBooksReleaseDateBefore(date).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public List<Author> findAllOrderByBooks() {
+        return this.authorRepository.findAllDistinctOrderByBooks().orElseThrow(NoSuchElementException::new);
     }
 
 }
