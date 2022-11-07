7fa3a9fd091b9a209ed020968eb2f7b03d935674
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-07 21:44:13 +0200

    wip

--- src/main/java/com/example/bookshop/repositories/BookRepository.java
+++ src/main/java/com/example/bookshop/repositories/BookRepository.java
@@ -4,6 +4,10 @@ import com.example.bookshop.domain.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
