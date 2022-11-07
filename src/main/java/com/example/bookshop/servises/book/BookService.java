f7dc252f57b7a573c0c2aa1bf429aacf4092b40d
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-07 20:29:51 +0200

    wip

--- src/main/java/com/example/bookshop/servises/book/BookService.java
+++ src/main/java/com/example/bookshop/servises/book/BookService.java
@@ -6,4 +6,10 @@ import java.util.List;
 
 public interface BookService {
     void seedBooks(List<Book> books);
+
+    List<Book> findAllByReleaseDateAfter(LocalDate localDate);
+
+
+
+    List<Book> findAllByAuthorFirstNameAndAuthorLastNameOrderByReleaseDateDescTitleAsc(String firstName, String lastName);
 }
