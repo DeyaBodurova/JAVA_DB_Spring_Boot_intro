9540657e46b9dc93f9a82c8a3f26cf94931cadc8
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-07 20:34:47 +0200

    wip

--- src/main/java/com/example/bookshop/domain/entities/Author.java
+++ src/main/java/com/example/bookshop/domain/entities/Author.java
@@ -2,10 +2,7 @@ package com.example.bookshop.domain.entities;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Set;
 
 @Getter
@@ -16,10 +13,17 @@ import java.util.Set;
 @Entity
 @Table
 public class Author extends BaseEntity {
-    @Column(name = "fist_name")
+
+    @Column(name = "first_name")
     private String firstName;
+
     @Column(name = "last_name", nullable = false)
     private String lastName;
-    @OneToMany(targetEntity = Book.class, mappedBy = "author")
+
+    @OneToMany(targetEntity = Book.class, mappedBy = "author", fetch = FetchType.EAGER)
     private Set<Book> books;
+
+    public String toStringWithCount() {
+        return String.format("%s %s %d", firstName, lastName, books.size());
+    }
 }
