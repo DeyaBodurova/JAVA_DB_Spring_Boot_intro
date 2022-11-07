63d4752b2750282ccd8685c34a5f68f06854c96a
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-07 20:36:19 +0200

    wip

--- src/main/java/com/example/bookshop/ConsoleRunner.java
+++ src/main/java/com/example/bookshop/ConsoleRunner.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class ConsoleRunner implements CommandLineRunner {
 
