55b186f8e3130c813b8846f6c8c01e75b388587c
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-07 19:13:41 +0200

    wip

--- src/main/java/com/example/bookshop/servises/seed/SeedServiceImpl.java
+++ src/main/java/com/example/bookshop/servises/seed/SeedServiceImpl.java
@@ -6,28 +6,69 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SeedServiceImpl implements SeedService {
-    private final SeedService seedService;
+    private final AuthorService authorService;
     private final BookService bookService;
     private final CategoryService categoryService;
 
-    public SeedServiceImpl(SeedService seedService, BookService bookService, CategoryService categoryService) {
-        this.seedService = seedService;
+    public SeedServiceImpl(AuthorService authorService, BookService bookService, CategoryService categoryService) {
+        this.authorService = authorService;
         this.bookService = bookService;
         this.categoryService = categoryService;
     }
 
     @Override
-    public void seedAuthors() {
-        RESOUR
+    public void seedAuthors() throws IOException {
+        if (!this.authorService.isDataSeeded()) {
+            this.authorService
+                    .seedAuthors(Files.readAllLines(Path.of(RESOURCE_URL + AUTHOR_FILE_NAME))
+                            .stream()
+                            .filter(s -> !s.isBlank())
+                            .map(firstAndLastName -> Author.builder()
+                                    .firstName(firstAndLastName.split(" ")[0])
+                                    .lastName(firstAndLastName.split(" ")[1])
+                                    .build())
+                            .collect(Collectors.toList()));
+        }
     }
 
     @Override
-    public void seedBooks() {
+    public void seedBooks() throws IOException {
+        final List<Book> books = Files.readAllLines(Path.of(RESOURCE_URL + BOOK_FILE_NAME))
+                .stream()
+                .filter(s -> !s.isBlank())
+                .map(row -> {
+                    String[] data = row.split("\\s+");
+                    String title = Arrays.stream(data)
+                            .skip(5)
+                            .collect(Collectors.joining(" "));
 
+                    return Book.builder()
+                            .title(title)
+                            .editionType(EditionType.values()[Integer.parseInt(data[0])])
+                            .price(new BigDecimal(data[3]))
+                            .releaseDate(LocalDate.parse(data[1], DateTimeFormatter.ofPattern("d/M/yyyy")))
+                            .ageRestriction(AgeRestriction.values()[Integer.parseInt(data[4])])
+                            .author(this.authorService.getRandomAuthor())
+                            .categories(this.categoryService.getRandomCategories())
+                            .copies(Integer.parseInt(data[2]))
+                            .build();
+
+                })
+                .collect(Collectors.toList());
+
+        this.bookService.seedBooks(books);
     }
 
     @Override
-    public void seedCategory() {
-
+    public void seedCategory() throws IOException {
+        if (!this.categoryService.isDataSeeded()) {
+            this.categoryService.seedCategories(Files.readAllLines(Path.of(RESOURCE_URL + CATEGORY_FILE_NAME))
+                    .stream()
+                    .filter(s -> !s.isBlank())
+                    .map(name -> Category.builder()
+                            .name(name)
+                            .build())
+                    .collect(Collectors.toList()));
+        }
     }
 }
