288e93efa08becc2a1d889db14d04b2c7f60979b
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-07 20:35:29 +0200

    wip

--- src/main/java/com/example/bookshop/Constants/FilePath.java
+++ src/main/java/com/example/bookshop/Constants/FilePath.java
@@ -3,7 +3,11 @@ package com.example.bookshop.Constants;
 public enum FilePath {
     ;
     public static final String RESOURCE_URL = "src/main/resources/dbContent/";
+
     public static final String BOOK_FILE_NAME = "books.txt";
+
     public static final String CATEGORY_FILE_NAME = "categories.txt";
+
     public static final String AUTHOR_FILE_NAME = "authors.txt";
+
 }
