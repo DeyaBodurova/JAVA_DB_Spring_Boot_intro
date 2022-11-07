3233341f0ed83f607721e37920500f5574e98bf1
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-07 20:30:59 +0200

    wip

--- src/main/java/com/example/bookshop/servises/author/AuthorService.java
+++ src/main/java/com/example/bookshop/servises/author/AuthorService.java
@@ -5,5 +5,15 @@ import com.example.bookshop.domain.entities.Author;
 import java.util.List;
 
 public interface AuthorService {
+
     void seedAuthors(List<Author> authors);
+
+    boolean isDataSeeded();
+
+    Author getRandomAuthor();
+
+    List<Author> findDistinctByBooksBefore(LocalDate date);
+
+    List<Author> findAllOrderByBooks();
+
 }
